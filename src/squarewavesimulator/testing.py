f700cfae9614ff492a75ff7dc8b682e4f450456c
Steven Linfield <S.Linfield@outlook.com>
2023-03-17 00:13:48 +0000

    Seemed to fix boundary conditions and flux

--- src/squarewavesimulator/testing.py
+++ src/squarewavesimulator/testing.py
@@ -11,9 +11,9 @@ from scipy.integrate import solve_ivp as solver
 
 class E:
     """Simulation of an E mechanism using solving from scipy \n
-    E: A -> B + e"""
+    E: R -> O + e"""
 
-    def __init__(self, input, E0, k0, a, cA, cB, DA, DB, r, expansion):
+    def __init__(self, input, E0, k0, a, cR, cO, DR, DO, r, expansion, Nernstian, BV, MH):
         '''Waveform variables'''
         self.input = input
         self.index = self.input.index
@@ -31,36 +31,47 @@ class E:
         self.E0 = E0
         self.k0 = k0
         self.a = a
-        self.cA = cA
-        self.cB = cB
-        self.DA = DA
-        self.DB = DB
+        self.cR = cR
+        self.cO = cO
+        self.DR = DR
+        self.DO = DO
 
         self.F = 96485
         self.R = 8.314
         self.Temp = 298
+        
+        self.Nernstian = Nernstian
+        self.BV = BV
+        self.MH = MH
+
+        self.methods = 0
+        for ix in [self.Nernstian, self.BV, self.MH]:
+            if ix == True:
+                self.methods += 1
+
+        if self.methods == 0:
+            print('\n' + 'No kinetic model was chosen' + '\n')
+            sys.exit()
+
+        if self.methods >= 2:
+            print('\n' + 'More than one kinetic model was chosen' + '\n')
+            sys.exit()
 
         '''Spatial grid variables'''
         self.r = r
         self.expansion = expansion
 
         '''Dimensionless variables''' 
-        if self.cA >= self.cB:
-            self.cmax = self.cA
-        elif self.cA < self.cB:
-            self.cmax = self.cB
-        
-        self.CA = self.cA / self.cmax
-        self.CB = self.cB / self.cmax
+        concentrations = np.array([self.cR, self.cO])
+        self.cmax = np.amax(concentrations)       
+        self.CR = self.cR / self.cmax
+        self.CO = self.cO / self.cmax
 
+        diffusions = np.array([self.DR, self.DO])
+        self.Dmax = np.amax(diffusions)
 
-        if self.DA >= self.DB:
-            self.Dmax = self.DA
-        elif self.DA < self.DB:
-            self.Dmax = self.DB
-
-        self.dA = self.DA / self.Dmax
-        self.dB = self.DB / self.Dmax     
+        self.dR = self.DR / self.Dmax
+        self.dO = self.DO / self.Dmax  
         self.d = self.Dmax / self.Dmax
 
         self.T = (self.Dmax * self.t) / (self.r ** 2)
@@ -83,49 +94,73 @@ class E:
         self.n = int(self.x.size) 
         self.m = int(self.theta.size)
 
-        self.C = np.ones((self.n, self.m)) * self.CA
+        self.C_R = np.ones((self.n, self.m)) * self.CR
+        self.C_O = np.ones((self.n, self.m)) * self.CO
 
-        self.alpha = np.ones(self.n - 1)
-        self.beta = np.ones(self.n)
-        self.gamma = np.ones(self.n - 1)
-            
+        self.alpha_R = np.ones(self.n - 1)
+        self.beta_R = np.ones(self.n)
+        self.gamma_R = np.ones(self.n - 1)
+        
+        self.alpha_O = np.ones(self.n - 1)
+        self.beta_O = np.ones(self.n)
+        self.gamma_O = np.ones(self.n - 1)            
           
         for ix in range(1, self.n - 1):
             self.xplus = self.x[ix + 1] - self.x[ix]
             self.xminus = self.x[ix] - self.x[ix - 1]
             self.denominator = self.xminus * (self.xplus ** 2) + self.xplus * (self.xminus **2)
-            self.alpha[ix - 1] *= 2 * self.xplus / self.denominator
-            self.beta[ix] *= -2 * (self.xminus + self.xplus) / self.denominator
-            self.gamma[ix] *= 2 * self.xminus / self.denominator
-            #could fix this up so it is making proper matrix
+            
+            self.alpha_R[ix - 1] *= 2 * self.xplus / self.denominator
+            self.beta_R[ix] *= -2 * (self.xminus + self.xplus) / self.denominator
+            self.gamma_R[ix] *= 2 * self.xminus / self.denominator
+            
+            self.alpha_O[ix - 1] *= 2 * self.xplus / self.denominator
+            self.beta_O[ix] *= -2 * (self.xminus + self.xplus) / self.denominator
+            self.gamma_O[ix] *= 2 * self.xminus / self.denominator
         
             
 
-        A = diagonals([self.alpha, self.beta, self.gamma], [-1,0,1]).toarray()
-        A[0,:] = np.zeros(self.n)
-        A[0,0] = 1     
+        R = diagonals([self.alpha_R, self.beta_R, self.gamma_R], [-1,0,1]).toarray()
+        R[0,:] = np.zeros(self.n)
+        R[0,0] = 1     
+        
+        O = diagonals([self.alpha_O, self.beta_O, self.gamma_O], [-1,0,1]).toarray()
+        O[0,:] = np.zeros(self.n)
+        O[0,0] = 1   
 
-        def function(t,y):
-            return np.dot(A,y)
+        def reduced(t,y):
+            return np.dot(R,y)
+        
+        def oxidised(t,y):
+            return np.dot(O,y)
 
         self.flux = np.array([])
-
         for k in range(1,self.m):
-            self.C[0, k] = (self.C[1, k - 1] + (self.x[1] - self.x[0]) * self.K0 * np.exp(-self.a * self.theta[k - 1]))/(1 + (self.x[1] - self.x[0]) * self.K0 * (np.exp((1 - self.a) * self.theta[k - 1]) + np.exp(-self.a * self.theta[k - 1])))
-            
-            integrator = solver(function, [0, self.dT], self.C[:,k - 1], t_eval=[self.dT], method='RK45')
-            
-            self.C[1:-1, k] = integrator.y[1:-1, 0]
+            '''Boundary conditions'''
+            if self.Nernstian == True:
+                '''Nernstian'''
+                self.C_R[0, k] = (self.C_R[1, k - 1] + (self.dR/self.dO) * self.C_O[1, k - 1])/(1 + (self.dR/self.dO) * np.exp(self.theta[k-1]))
 
+                self.C_O[0, k] = (self.C_R[1, k - 1] + (self.dR/self.dO) * self.C_O[1, k - 1])/((self.dR/self.dO) + np.exp(-self.theta[k-1]))
+            
+            if self.BV == True:
+                '''Butler-Volmer'''
+                self.C_R[0, k] = (-self.C_R[1, k - 1] + (self.x[1] - self.x[0]) * self.K0 * np.exp(-self.a * self.theta[k - 1]) * (self.C_O[1, k - 1] + (self.dR/self.dO) * self.C_R[1, k - 1]))/((self.x[1] - self.x[0]) * self.K0 * (np.exp((1 - self.a) * self.theta[k - 1]) + (self.dR/self.dO) * np.exp((-self.a) * self.theta[k - 1])) - 1)
+
+                self.C_O[0, k] = (-self.C_O[1, k - 1] + (self.x[1] - self.x[0]) * self.K0 * np.exp((1 - self.a) * self.theta[k - 1]) * (self.C_R[1, k - 1] + (self.dO/self.dR) * self.C_O[1, k - 1]))/((self.x[1] - self.x[0]) * self.K0 * (np.exp(-self.a * self.theta[k - 1]) + (self.dO/self.dR) * np.exp((1 - self.a) * self.theta[k - 1])) - 1)
+           
+            oxidation = solver(reduced, [0, self.dT], self.C_R[:,k - 1], t_eval=[self.dT], method='RK45')
+            self.C_R[1:-1, k] = oxidation.y[1:-1, 0]
+            
+            reduction = solver(oxidised, [0, self.dT], self.C_O[:,k - 1], t_eval=[self.dT], method='RK45')
+            self.C_O[1:-1, k] = reduction.y[1:-1, 0]
                             
-            self.flux = np.append(self.flux, (self.F * np.pi * self.r * self.cA * self.DA) * ((self.C[1, k] - self.C[0, k]) / (self.x[1] - self.x[0]))) # did I save flux properly, or is something still wrong with BV?
+            self.flux = np.append(self.flux, (self.F * np.pi * self.r * self.cR * self.DR) * ((self.C_R[1, k] - self.C_R[0, k]) / (self.x[1] - self.x[0])) - (self.F * np.pi * self.r * self.cO * self.DO) * ((self.C_O[1, k] - self.C_O[0, k]) / (self.x[1] - self.x[0])))
 
         self.output = zip(self.E, self.flux)
-        self.simend = time.time()
     
     def results(self):
         return self.output
-
 
 if __name__ == '__main__':
     
@@ -140,10 +175,10 @@ if __name__ == '__main__':
         else: 
             raise
     
-    shape = wf.CV(Eini = 0, Eupp = 0.5, Elow = 0.0, dE = 0.002,sr = 0.1, ns = 1)
-    instance = E(input = shape, E0 = 0.25, k0 = 0.01, a = 0.5, cA = 0.005, cB = 0.000, DA = 5E-6, DB = 5E-6, r = 0.15, expansion = 1.05)
+    shape = wf.CV(Eini = 0.0, Eupp = 0.5, Elow = 0.0, dE = 0.002, sr = 0.01, ns = 1)
+    instance = E(input = shape, E0 = 0.25, k0 = 0.1, a = 0.5, cR = 0.005, cO = 0.000, DR = 5E-6, DO = 5E-6, r = 0.15, expansion = 1.05, Nernstian = False, BV = True, MH = False)
     
-    filepath = cwd + '/data/' + 'test K1 ' + '.txt'
+    filepath = cwd + '/data/' + 'DO 5E-6' + '.txt'
     with open(filepath, 'w') as file:
         for ix, iy in instance.results():
             file.write(str(ix) + ',' + str(iy) + '\n')
