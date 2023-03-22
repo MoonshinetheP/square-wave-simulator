0288b3333d1bf726227cb0e5b0a0f52afbb41904
Steven Linfield <S.Linfield@outlook.com>
2023-03-22 20:00:12 +0100

    Updated E to deal with either pulse or sweep

--- src/electrochemistrysimulations/E.py
+++ src/electrochemistrysimulations/E.py
@@ -19,13 +19,30 @@ class E:
         self.index = self.input.index
         self.t = self.input.t
         self.E = self.input.E
-        
-        self.Eini = self.input.Eini
-        self.Eupp = self.input.Eupp
-        self.Elow = self.input.Elow
-        self.dE = self.input.dE
-        self.sr = self.input.sr
-        self.ns = self.input.ns
+        self.tPLOT = self.input.tPLOT
+        self.EPLOT = self.input.EPLOT
+
+        if self.input.type == 'sweep':
+            self.Eini = self.input.Eini
+            self.Eupp = self.input.Eupp
+            self.Elow = self.input.Elow
+            self.dE = self.input.dE
+            self.sr = self.input.sr
+            self.ns = self.input.ns
+
+            self.detailed = False
+
+        if self.input.type == 'pulse':
+            self.Eini = self.input.Eini
+            self.Efin = self.input.Efin
+            self.dEs = self.input.dEs
+            self.dEp = self.input.dEp
+            self.pt = self.input.pt
+            self.rt = self.input.rt
+            self.st = self.input.st
+            self.detailed = self.input.detailed
+
+
         
         '''Mechanism variables'''
         self.E0 = E0
@@ -75,8 +92,14 @@ class E:
         self.d = self.Dmax / self.Dmax
 
         self.T = (self.Dmax * self.t) / (self.r ** 2)
-        self.dT = self.T[1] - self.T[0]      
+        self.dT = np.diff(self.T)
+        
+        if self.detailed == False:
+            self.sT = self.dT
+        else:
+            self.sT = np.ones((self.dT.size)) * (self.Dmax * self.st) / (self.r ** 2)
         self.Tmax = self.T[-1] 
+        
 
         self.Xmax = 6 * np.sqrt(self.d * self.Tmax)
 
@@ -85,7 +108,7 @@ class E:
         self.K0 = (self.k0 * self.r) / self.Dmax
         
         '''Expanding spatial grid'''                 
-        self.dX = np.sqrt(2.05 * self.dT)
+        self.dX = np.sqrt(2.05 * self.dT[0])
         self.x = np.array([0])
         while self.x[-1] < self.Xmax:
             self.x = np.append(self.x, self.x[-1] + self.dX)
@@ -134,7 +157,9 @@ class E:
         def oxidised(t,y):
             return np.dot(O,y)
 
-        self.flux = np.array([])
+        self.flux = np.array([0])
+        self.fluxplus = np.array([])
+        self.fluxminus = np.array([])
         for k in range(1,self.m):
             '''Boundary conditions'''
             if self.Nernstian == True:
@@ -149,15 +174,25 @@ class E:
 
                 self.C_O[0, k] = (-self.C_O[1, k - 1] + (self.x[1] - self.x[0]) * self.K0 * np.exp((1 - self.a) * self.theta[k - 1]) * (self.C_R[1, k - 1] + (self.dO/self.dR) * self.C_O[1, k - 1]))/((self.x[1] - self.x[0]) * self.K0 * (np.exp(-self.a * self.theta[k - 1]) + (self.dO/self.dR) * np.exp((1 - self.a) * self.theta[k - 1])) - 1)
            
-            oxidation = solver(reduced, [0, self.dT], self.C_R[:,k - 1], t_eval=[self.dT], method='RK45')
+            oxidation = solver(reduced, [0, self.dT[k - 1]], self.C_R[:,k - 1], t_eval=[self.sT[k - 1]], method='RK45')
             self.C_R[1:-1, k] = oxidation.y[1:-1, 0]
             
-            reduction = solver(oxidised, [0, self.dT], self.C_O[:,k - 1], t_eval=[self.dT], method='RK45')
+            reduction = solver(oxidised, [0, self.dT[k - 1]], self.C_O[:,k - 1], t_eval=[self.sT[k - 1]], method='RK45')
             self.C_O[1:-1, k] = reduction.y[1:-1, 0]
-                            
-            self.flux = np.append(self.flux, (self.F * np.pi * self.r * self.cR * self.DR) * ((self.C_R[1, k] - self.C_R[0, k]) / (self.x[1] - self.x[0])) - (self.F * np.pi * self.r * self.cO * self.DO) * ((self.C_O[1, k] - self.C_O[0, k]) / (self.x[1] - self.x[0])))
+            
+            if self.input.type == 'sweep':
+                self.flux = np.append(self.flux, (self.F * np.pi * self.r * self.cR * self.DR) * ((self.C_R[1, k] - self.C_R[0, k]) / (self.x[1] - self.x[0])) - (self.F * np.pi * self.r * self.cO * self.DO) * ((self.C_O[1, k] - self.C_O[0, k]) / (self.x[1] - self.x[0])))
+            
+            if self.input.type == 'pulse':
+                if k % 2 != 0:
+                    self.fluxplus = np.append(self.fluxplus, (self.F * np.pi * self.r * self.cR * self.DR) * ((self.C_R[1, k] - self.C_R[0, k]) / (self.x[1] - self.x[0])) - (self.F * np.pi * self.r * self.cO * self.DO) * ((self.C_O[1, k] - self.C_O[0, k]) / (self.x[1] - self.x[0])))
+                if k % 2 == 0:
+                    self.fluxminus = np.append(self.fluxminus, (self.F * np.pi * self.r * self.cR * self.DR) * ((self.C_R[1, k] - self.C_R[0, k]) / (self.x[1] - self.x[0])) - (self.F * np.pi * self.r * self.cO * self.DO) * ((self.C_O[1, k] - self.C_O[0, k]) / (self.x[1] - self.x[0])))
 
-        self.output = zip(self.t, self.E, self.flux)
+        if self.input.type == 'pulse':
+            self.flux = self.fluxplus - self.fluxminus
+
+        self.output = zip(self.tPLOT, self.EPLOT, self.flux)
     
     def results(self):
         return self.output
@@ -176,10 +211,10 @@ if __name__ == '__main__':
         else: 
             raise
     
-    shape = wf.CV(Eini = 0.0, Eupp = 0.5, Elow = 0.0, dE = 0.002, sr = 0.01, ns = 1)
-    instance = E(input = shape, E0 = 0.25, k0 = 0.1, a = 0.5, cR = 0.005, cO = 0.000, DR = 7.5E-6, DO = 5E-6, r = 0.15, expansion = 1.05, Nernstian = False, BV = True, MH = False)
+    shape = wf.DPV(Eini = 0, Efin = 0.5, dEs = 0.005, dEp = 0.01, pt = 0.01, rt = 0.03, st = 0.000001, detailed = False)
+    instance = E(input = shape, E0 = 0.25, k0 = 0.1, a = 0.5, cR = 0.005, cO = 0.000, DR = 5E-6, DO = 5E-6, r = 0.15, expansion = 1.05, Nernstian = False, BV = True, MH = False)
     
-    filepath = cwd + '/data/' + 'DR 7.5E-6' + '.txt'
+    filepath = cwd + '/data/' + 'pulse' + '.txt'
     with open(filepath, 'w') as file:
         for ix, iy, iz in instance.results():
             file.write(str(ix) + ',' + str(iy) + ',' + str(iz) + '\n')
