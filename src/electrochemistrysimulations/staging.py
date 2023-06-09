7a2b439110311fd8a381a9ffcc76cc1717277ff4
Steven Linfield <S.Linfield@outlook.com>
2023-06-09 10:38:49 +0300

    Replacement for testing.py

---
+++ src/electrochemistrysimulations/staging.py
@@ -0,0 +1,28 @@
+'''
+===================================================================================================
+Copyright (C) 2023 Steven Linfield
+
+This file is part of the electrochemistry-simulations package. This package is free software: you 
+can redistribute it and/or modify it under the terms of the GNU General Public License as published 
+by the Free Software Foundation, either version 3 of the License, or (at your option) any later 
+version. This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+GNU General Public License for more details. You should have received a copy of the GNU General 
+Public License along with electrochemistry-simulations. If not, see https://www.gnu.org/licenses/
+===================================================================================================
+
+Package title:      electrochemistry-simulations
+Repository:         https://github.com/MoonshinetheP/electrochemistry-simulations
+Date of creation:   09/03/2023
+Main author:        Steven Linfield (MoonshinetheP)
+Collaborators:      None
+Acknowledgements:   Oliver Rodriguez (oliverrdz)
+    
+Filename:           staging.py
+
+===================================================================================================
+How to use this file:
+    
+
+===================================================================================================
+'''
